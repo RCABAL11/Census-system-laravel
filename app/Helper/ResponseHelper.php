48df7fd49c515b691b633d67e37bb7661225b12c
RCABAL11 <reymartcabal46@gmail.com>
2024-08-22 22:26:43 +0800

    with responseHelper

---
+++ app/Helper/ResponseHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helper;
+
+class ResponseHelper
+{
+    /**
+     * Create a new class instance.
+     */
+    public static function success($status = 'success', $message = NULL, $data = [], $statusCode = 200)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ], $statusCode);
+    }
+
+    public static function errors($status = 'error', $message = NULL, $statusCode = 400)
+    {
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+        ], $statusCode);
+    }
+}
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,27 +6,30 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Helper\ResponseHelper;
+use Illuminate\Support\Facades\Auth;
+use Exception;
 
 class RegisterController extends Controller
 {
     public function register(RegisterRequest $request)
-    {
-
+{
+    try {
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        return response()->json([
-            'message' => 'User registered successfully',
-            'user' => [
-                'id' => $user->id,
-                'name' => $user->name,
-                'email' => $user->email,
-                'created_at' => $user->created_at->toDateTimeString(),
-                'updated_at' => $user->updated_at->toDateTimeString(),
-            ]
-        ], 201);
+        if ($user) {
+            return ResponseHelper::success(message: 'User has been registered successfully!', data: $user, statusCode: 201);
+        }
+
+        return ResponseHelper::errors(message: 'Unable to Register User! Please try again.', statusCode: 400);
+    } catch (Exception $e) {
+        \Log::error('Unable to Register User: ' . $e->getMessage() . ' - Line no. ' . $e->getLine());
+        return ResponseHelper::error(message: 'An error occurred while registering the user.', statusCode: 500);
     }
+}
+
 }
